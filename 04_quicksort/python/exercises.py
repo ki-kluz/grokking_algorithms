0c05fd9e255c9829d12948ae11dbcec82f4969d1
kirillkoluzaev <kirill.koluzaev06@gmail.com>
2026-07-06 23:37:38 +0300

    Разбор стратегии: «Разделяй и властвуй»

---
+++ 04_quicksort/python/exercises.py
@@ -0,0 +1,29 @@
+# Функция sum (через рекурсию)
+def sum(arr):
+	if not arr:
+		return 0
+	else:
+		return arr[0] + sum(arr[1:])
+
+# print(sum([1, 2, 11, 13]))
+
+# Функция count (через рекурсию)
+def count(arr):
+	if not arr:
+		return 0
+	else:
+		return 1 + count(arr[1:])
+	
+# print(count([1, 2, 3]))
+
+# Функция поиска НАИБОЛЬШЕГО числа (через рекурсию)
+def max(arr):
+	if not arr:
+		return None
+	elif len(arr) == 1:
+		return arr[0]
+	else:
+		rest_max = max(arr[1:])
+		return arr[0] if arr[0] > rest_max else rest_max
+	
+# print(max([1, 2, 3, -1, 0, 1]))
